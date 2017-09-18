d172a17ecbbc7bd2ade755bd7ea8705463f01a77
Alakbar Valiyev <v_elekber@hotmail.com>
2017-09-18 17:44:47 +0400

    Publish source.

---
+++ XmlToHtml/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Xml.Xsl;
+
+namespace XmlToHtml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //If not passed file paths to .xml, .xsl , .html files show usage
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Usage: xml2html.exe xslfile.xsl xmlfile.xml output.html");
+                return;
+            }
+
+            //Check if exist xml & xsl files.
+            if (!File.Exists(args[0]) || !File.Exists(args[1])) 
+            {
+                Console.WriteLine(args[0] + " or " + args[1] + " doesn't exist");
+                return;
+            }
+            XslCompiledTransform xsl = new XslCompiledTransform();
+            String outputFile = args[2];
+            xsl.Load(args[0]);
+            xsl.Transform(args[1], args[2]);
+
+            if (File.Exists(args[2])) Console.WriteLine(args[2] + " successfully created!");
+            else Console.WriteLine("Couldn't create " + args[2]);
+            
+        }
+    }
+}
